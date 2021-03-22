3311c7af86e2d281e8e344696ae198b4d66d0bcc
skypekitten9 <victor.enhorning@gmail.com>
2021-03-22 13:48:02 +0100

    Added derived gamemode blueprint and created grabber componant.

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameFramework/PlayerController.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber ready for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPos, 
+		OUT PlayerViewRotator
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Pos is %s and rotation is %s."), 
+		*PlayerViewPos.ToString(), 
+		*PlayerViewRotator.ToString()
+	);
+}
+
